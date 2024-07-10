817b698ba8e907114e2145eaffbe4852463f40d0
MaryJudit <maryjudit6@gmail.com>
2024-07-10 13:19:59 +0530

    Initial Commit

---
+++ JuditBusinessCard/JuditBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuditBusinessCardApp.swift
+//  JuditBusinessCard
+//
+//  Created by Mary Judit Yesudasan on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct JuditBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
